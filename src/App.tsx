ac21e67c08a98c4a661198cdb91f9f56f5cb67b8
Colki <mycolki@gmail.com>
2023-01-30 19:47:40 +0900

    feat: axios, react-query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { PageLayout } from 'components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Routes from 'pages/Routes';
 import Global from './Global';
 
+const client = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={client}>
       <Global />
       <PageLayout>
         <Routes />
       </PageLayout>
-    </>
+    </QueryClientProvider>
   );
 }
 
