0eeaf0d58dd6adc01b880e8d3a367a8beec4effa
Colki <mycolki@gmail.com>
2023-01-30 16:19:02 +0900

    chore: dayjs 세팅

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,15 @@
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import 'dayjs/locale/ko';
+
+dayjs.extend(utc).locale('ko');
+
+type FormatType = 'HH:mm' | 'YYYY년 MM월 DD일' | 'MM월 DD일' | 'ddd' | 'YYYY.MM.DD';
+
+export function formatDateToLocale(date: string, formatType: FormatType) {
+  return dayjs(date).format(formatType);
+}
+
+export function formatDateToUTC() {
+  return dayjs.utc().format();
+}
